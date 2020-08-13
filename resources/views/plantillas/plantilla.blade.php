9aae39bedc2191e9492463eac44103c184da9805
frezeer <frezee13@hotmail.com>
2020-08-13 00:45:36 -0500

    se combina laravel mix se excluyen assets en la esctructura y se implementa laravel vue

--- resources/views/plantillas/plantilla.blade.php
+++ resources/views/plantillas/plantilla.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 
 <title>@yield('titulo')</title>
@@ -9,16 +9,21 @@
 <meta name="description" content="Jhonatan Shop template">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="stylesheet" type="text/css" href="{{ asset('asset/styles/bootstrap-4.1.2/bootstrap.min.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('asset/plugins/font-awesome-4.7.0/css/font-awesome.min.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('asset/plugins/OwlCarousel2-2.2.1/owl.carousel.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('asset/plugins/OwlCarousel2-2.2.1/owl.theme.default.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('asset/plugins/OwlCarousel2-2.2.1/animate.css') }}">
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
+<!-- Styles -->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+<!-- Styles -->
+<link href="{{ asset('css/all.css') }}" rel="stylesheet">
+
+<link rel="stylesheet" type="text/css" href="{{ asset('asset/plugins/font-awesome-4.7.0/css/font-awesome.min.css') }}">
 
 <link rel="stylesheet" type="text/css" href="{{ asset('asset/styles/main_styles.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('asset/styles/responsive.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('asset/styles/comun.css') }}">
+
+
 
 </head>
 <body>
@@ -208,18 +213,11 @@
 		
 </div>
 
-<script src=" {{ asset('asset/js/jquery-3.2.1.min.js') }}"></script>
-<script src=" {{ asset('asset/styles/bootstrap-4.1.2/popper.js') }}"></script>
-<script src=" {{ asset('asset/styles/bootstrap-4.1.2/bootstrap.min.js') }}"></script>
-<script src=" {{ asset('asset/plugins/greensock/TweenMax.min.js') }}"></script>
-<script src=" {{ asset('asset/plugins/greensock/TimelineMax.min.js') }}"></script>
-<script src=" {{ asset('asset/plugins/scrollmagic/ScrollMagic.min.js') }}"></script>
-<script src=" {{ asset('asset/plugins/greensock/animation.gsap.min.js') }}"></script>
-<script src=" {{ asset('asset/plugins/greensock/ScrollToPlugin.min.js') }}"></script>
-<script src=" {{ asset('asset/plugins/OwlCarousel2-2.2.1/owl.carousel.js') }}"></script>
-<script src=" {{ asset('asset/plugins/easing/easing.js') }}"></script>
-<script src=" {{ asset('asset/plugins/progressbar/progressbar.min.js') }}"></script>
-<script src=" {{ asset('asset/plugins/parallax-js-master/parallax.min.js') }}"></script>
-<script src=" {{ asset('asset/js/custom.js')}}"></script>
+	<script src=" {{ asset('asset/js/jquery-3.2.1.min.js') }}"></script>
+	  <!-- Scripts -->
+	<script src="{{ asset('js/app.js') }}" defer></script>
+	<!-- Scripts -->
+	<script src="{{ asset('js/all.js') }}" defer></script>
+
 </body>
 </html>
